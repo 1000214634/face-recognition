6ac536e6b518bae53566c9b91af43b7ca73487b8
Ahmed Zaki <87226926+1000214634@users.noreply.github.com>
2024-03-17 01:27:52 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install required dependencies
+pip install -r requirements.txt
+
+# Optionally, you can add any other setup tasks here, such as database migrations or model downloads.
