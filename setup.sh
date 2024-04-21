4f96a40fc0d01e2ac79c4314f63cf4636ca166ce
Ahmed Zaki <87226926+1000214634@users.noreply.github.com>
2024-04-21 17:19:54 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install required dependencies
-pip install -r requirements.txt
-
-# Optionally, you can add any other setup tasks here, such as database migrations or model downloads.
